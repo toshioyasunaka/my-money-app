be7a93a829ee8ca525f46c0526d12fa234eb73f7
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2023-03-10 08:47:26 -0300

    addition of footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const footer = () => {
+    return (
+        <footer className="main-footer">
+            <strong>
+                Copyright &copy; 2017
+                <a href="http://cod3r.com.br" target='_blank'>Cod3r</a>.
+            </strong>
+        </footer>
+    )
+}
+
+export default footer
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -3,12 +3,18 @@ import React from "react"
 import '../common/template/dependencies'
 import Header from '../common/template/header'
 import Sidebar from "../common/template/sidebar"
+import Footer from "../common/template/footer"
+
 
 const App = props => {
     return (
         <div className="wrapper">
             <Header />
             <Sidebar />
+            <div className="content-wrapper">
+                <h1>Conteúdo</h1>
+            </div>
+            <Footer />
         </div>
     )
 }
