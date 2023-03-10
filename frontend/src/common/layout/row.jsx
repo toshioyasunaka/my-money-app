9c46607085837e9a4be40b8d9c2d432affa5e1a8
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2023-03-10 11:45:52 -0300

    creation of row layout and fill the value box of dashboard

---
+++ frontend/src/common/layout/row.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const row = props => {
+    return (
+        <div className="row">{props.children}</div>
+    )
+}
+
+export default row
--- frontend/src/common/widget/valueBox.jsx
+++ frontend/src/common/widget/valueBox.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 import Grid from "../layout/grid";
 
-const valueBox = () => {
-    <Grid cols={props.cols}>
-        <div className={`small-box bg-${props.color}`}>
-            <div className="inner">
-                <h3>{props.value}</h3>
-                <p>{props.text}</p>
+const valueBox = props => {
+    return (
+        <Grid cols={props.cols}>
+            <div className={`small-box bg-${props.color}`}>
+                <div className="inner">
+                    <h3>{props.value}</h3>
+                    <p>{props.text}</p>
+                </div>
+                <div className="icon">
+                    <i className={`fa fa-${props.icon}`}></i>
+                </div>
             </div>
-            <div className="icon">
-                <i className={`fa fa-${props.icon}`}></i>
-            </div>
-        </div>
-    </Grid>
+        </Grid>
+    )
 }
 
 export default valueBox
