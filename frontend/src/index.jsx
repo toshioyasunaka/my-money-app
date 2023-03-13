28d10cfd10e4a4a2c987cdd3e626c9a3d0295d58
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2023-03-13 08:41:55 -0300

    Redux config

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
+import reducers from './main/reducers'
 import Routes from './main/routes'
 
-ReactDOM.render(<Routes />, document.getElementById('App'))
+const store = createStore(reducers)
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>
+, document.getElementById('App'))
