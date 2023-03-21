7b79fa9cb7544d1f645c0cd98aa95e7f5193704a
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2023-03-21 09:57:30 -0300

    adjust style of form inputs

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {reduxForm, Field} from 'redux-form'
+import LableAndInput from "../common/form/lableAndInput";
 
 class BillingCycleForm extends Component {
     render() {
@@ -8,9 +9,12 @@ class BillingCycleForm extends Component {
         return(
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name="name" component="input" />
-                    <Field name="month" component="input" />
-                    <Field name="year" component="input" />
+                    <Field name="name" component={LableAndInput} 
+                        label='Nome' cols='12 4' placeholder='Informe o nome' />
+                    <Field name="month" component={LableAndInput} type='number'
+                        label='Mês' cols='12 4' placeholder='Informe o mês' />
+                    <Field name="year" component={LableAndInput} type='number'
+                        label='Ano' cols='12 4' placeholder='Informe o ano' />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className="btn btn-primary">Submit</button>
