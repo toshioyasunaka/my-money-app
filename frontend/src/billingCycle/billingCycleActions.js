ab152cd6210d14936d8c33c6beeb75ba021c7444
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2023-03-22 10:25:10 -0300

    fix the submit button on update the changes - adjusts  to the create function and creation of submit and update functions

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -16,8 +16,17 @@ export function getList() {
 }
 
 export function create(values) {
+    return submit(values, 'post')
+}
+
+export function update(values) {
+    return submit(values, 'put')
+}
+
+function submit(values, method) {
     return dispatch => {
-        axios.post(`${BASE_URL}/billingCycles`, values)
+        const id = values._id ? values._id : ''
+        axios[method](`${BASE_URL}/billingCycles/${id}`, values)
             .then(resp => {toastr.success('Sucesso', 'Operação realizada com sucesso.')
                 dispatch(init())
             })
@@ -27,7 +36,6 @@ export function create(values) {
             type: 'TEMP',
         }
     }
-    
 }
 
 export function showUpdate(billingCycle) {
