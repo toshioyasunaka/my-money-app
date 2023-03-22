81c0c5c1c1b5848d66de5203e42489bd83746b92
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2023-03-22 11:50:12 -0300

    delete button creation

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -8,16 +8,16 @@ import LableAndInput from "../common/form/lableAndInput";
 
 class BillingCycleForm extends Component {
     render() {
-        const {handleSubmit} = this.props
+        const {handleSubmit, readOnly} = this.props
 
         return(
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name="name" component={LableAndInput} 
+                    <Field name="name" component={LableAndInput} readOnly={readOnly}
                         label='Nome' cols='12 4' placeholder='Informe o nome' />
-                    <Field name="month" component={LableAndInput} type='number'
+                    <Field name="month" component={LableAndInput} type='number' readOnly={readOnly}
                         label='Mês' cols='12 4' placeholder='Informe o mês' />
-                    <Field name="year" component={LableAndInput} type='number'
+                    <Field name="year" component={LableAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
                 </div>
                 <div className="box-footer">
